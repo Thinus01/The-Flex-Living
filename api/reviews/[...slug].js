ff28e47b5c717c7483d327839896b00e0309c441
Thinus Van De Venter <thinusvdv2001@gmail.com>
2025-08-26 07:38:13 +0200

    Delete api/reviews/[...slug].js

--- api/reviews/[...slug].js
+++
@@ -1,8 +0,0 @@
-// api/reviews/[...slug].js
-export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    method: req.method,
-    slug: req.query.slug, // e.g. ["mock","1001","approval"]
-  });
-}
